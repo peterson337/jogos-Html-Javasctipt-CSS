fc854fa6b6bdcf96e243ca4a17c026250f8738ea
Peterson Rodeigues <petersonpepi16@gmail.com>
2021-08-05 13:12:47 -0300

    update

--- js/jogo-da-velha.js
+++ js/jogo-da-velha.js
@@ -6,3 +6,28 @@ var jogando = true;
 var nivel=1;
 var jogadaCpu=1;
 var quemComeca=1;
+
+function jogar(p){
+    if((jogando)&&(quemJoga==0)){
+        switch(p){
+            case 1:
+                if(jogo[0][0]==""){
+                    jogo[0][0]="x";
+                    quemJoga=1;
+                }
+                break;
+        }
+    }
+}
+
+function inicia(){
+    jogando=true;
+    jogadaCpu=1;
+    jogo=[
+        ["","",""],
+        ["","",""],
+        ["","",""]
+    ];
+}
+
+window.addEventListener("load",inicia);
