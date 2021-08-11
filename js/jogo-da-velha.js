869a587e9719c2a9075017aae668c54fd0ba9520
Peterson Rodeigues <petersonpepi16@gmail.com>
2021-08-11 13:26:56 -0300

    update

--- js/jogo-da-velha.js
+++ js/jogo-da-velha.js
@@ -16,9 +16,67 @@ function jogar(p){
                     quemJoga=1;
                 }
                 break;
+
+                case 2:
+                    if(jogo[0][1]==""){
+                        jogo[0][1]="x";
+                        quemJoga=1;
+                    }
+                    break;
+
+                    case 3:
+                        if(jogo[0][2]==""){
+                            jogo[0][2]="x";
+                            quemJoga=1;
+                        }
+                        break;
+
+                        case 4:
+                            if(jogo[1][0]==""){
+                                jogo[1][0]="x";
+                                quemJoga=1;
+                            }
+                            break;
+
+                            case 5:
+                                if(jogo[1][1]==""){
+                                    jogo[1][1]="x";
+                                    quemJoga=1;
+                                }
+                                break;
+
+                                case 6:
+                                    if(jogo[1][2]==""){
+                                        jogo[1][2]="x";
+                                        quemJoga=1;
+                                    }
+                                    break;
+
+                                    case 7:
+                                        if(jogo[2][0]==""){
+                                            jogo[2][0]="x";
+                                            quemJoga=1;
+                                        }
+                                        break;
+
+                                        case 8:
+                                            if(jogo[2][1]==""){
+                                                jogo[2][1]="x";
+                                                quemJoga=1;
+                                            }
+                                            break;
+
+                                  /*Default */          case 9:
+                                                if(jogo[2][2]==""){
+                                                    jogo[2][2]="x";
+                                                    quemJoga=1;
+                                                }
+                                                break;
         }
     }
 }
+
+
 
 function inicia(){
     jogando=true;
